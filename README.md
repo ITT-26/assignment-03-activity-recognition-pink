c8eb28bf3afc3663bb3192bd7bc04161cd6e1216
Alphazerfall <40797290+Alphazerfall@users.noreply.github.com>
2026-05-11 16:22:15 +0200

    add setup instructions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/QwFWBwI4)
+
+# Activity Regognition
+
+## Setup
+1. Clone the repo and navigate to it via `cd assignment-03-activity-recognition-pink`.
+2. Set it up a virtual enviroment running `python -m venv .venv`.
+3. Activate the virtual environment using `.venv\Scripts\activate` on Windows and `source .venv/bin/activate` on Linux/Mac.
+4. Install the required dependencies via `pip install -r requirements.txt`.
--- gather_data.py
+++ gather_data.py
@@ -1,1 +1,30 @@
-# this program gathers sensor data
+import csv
+import time
+import sys
+from DIPPID import SensorUDP
+
+
+ACTIVITIES = ["running", "rowing", "lifting", "jumpingjacks"]
+SAMPLING_RATES = [20, 100]
+PLACEMENTS = ["hand", "pocket"]
+DURATION = 10  # seconds per recording
+
+PORT = 5700
+sensor = SensorUDP(PORT)
+
+while(True):
+    # print all capabilities of the sensor
+    print('capabilities: ', sensor.get_capabilities())
+
+    # check if the sensor has the 'accelerometer' capability
+    if(sensor.has_capability('accelerometer')):
+        # print whole accelerometer object (dictionary)
+        print('accelerometer data: ', sensor.get_value('accelerometer'))
+
+        # print only one accelerometer axis
+        print('accelerometer X: ', sensor.get_value('accelerometer')['x'])
+
+    # if sensor.has_capability('button_1'):
+    #     print('button_1: ', sensor.get_value('button_1'))
+
+    time.sleep(1)
